306ce881b50af1d8c6035383f31949a8de843d9a
aya <ayachin.ret@gmail.com>
2018-11-29 19:44:35 -0500

    Added model and storage

---
+++ Shop/Shop.Core/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Models
+{
+    public class Product
+    {
+       //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //[Key]
+        public string Id { get; set; }
+
+        [StringLength(20)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range(0, 100)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}

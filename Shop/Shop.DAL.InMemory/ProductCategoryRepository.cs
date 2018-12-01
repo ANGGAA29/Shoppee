07b60ffc0ad64b67be26e0e1c3292cd815337df9
aya <ayachin.ret@gmail.com>
2018-11-30 21:26:17 -0500

    Product grouping

---
+++ Shop/Shop.DAL.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,79 @@
+﻿using Shop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DAL.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> productCategories;
+
+        public ProductCategoryRepository()
+        {
+            productCategories = cache["productCategories"] as List<ProductCategory>;
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["productCategories"] = productCategories;
+        }
+
+        public void Insert(ProductCategory p)
+        {
+            productCategories.Add(p);
+        }
+
+        public void Update(ProductCategory p)
+        {
+            ProductCategory productToUpdate = productCategories.Find(x => x.Id == p.Id);
+            if (productToUpdate != null)
+            {
+                productToUpdate = p;
+            }
+            else
+            {
+                throw new Exception("ProductCategory not found");
+            }
+        }
+
+        public ProductCategory Find(string id)
+        {
+            ProductCategory productCategory = productCategories.Find(x => x.Id == id);
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception("ProductCategory not found");
+            }
+        }
+
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productCategories.AsQueryable();
+        }
+
+        public void Delete(string id)
+        {
+            ProductCategory productCategory = productCategories.Find(x => x.Id == id);
+            if (productCategory != null)
+            {
+                productCategories.Remove(productCategory);
+            }
+            else
+            {
+                throw new Exception("ProductCategory not found");
+            }
+        }
+    }
+}

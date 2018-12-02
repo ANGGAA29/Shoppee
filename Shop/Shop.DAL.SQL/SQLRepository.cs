1083a1661dfe64e4fa41e2d7b0197c740e016a18
aya <ayachin.ret@gmail.com>
2018-12-02 07:58:48 -0500

    Implemented SQL Repository

---
+++ Shop/Shop.DAL.SQL/SQLRepository.cs
@@ -0,0 +1,60 @@
+﻿using Shop.Core.Contracts;
+using Shop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DAL.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+        public SQLRepository(DataContext _context)
+        {
+            context = _context;
+            dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var t = Find(id);
+            if(context.Entry(t).State == EntityState.Detached)
+            {
+                dbSet.Attach(t);
+            }
+
+            dbSet.Remove(t);
+        }
+
+        public T Find(string id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T p)
+        {
+            dbSet.Add(p);
+        }
+
+        public void Update(T p)
+        {
+            dbSet.Attach(p);
+            context.Entry(p).State = EntityState.Modified;
+        }
+    }
+}

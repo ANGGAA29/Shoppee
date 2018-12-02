b4690bd81ad059a37de59bfa6f413d5d09b40953
aya <ayachin.ret@gmail.com>
2018-12-01 23:20:53 -0500

    EF initial

---
+++ Shop/Shop.DAL.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Shop.DAL.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Shop.DAL.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Shop.DAL.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
